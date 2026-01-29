ab6c7efe7cb3e6914eefa5627a5dfab1bef9dd61
andreia-berto <andreia@jaya.tech>
2026-01-29 17:06:17 -0300

    feat: obfuscate sensitive fields

--- app/core/logging/setup.py
+++ app/core/logging/setup.py
@@ -6,6 +6,14 @@ from structlog.stdlib import LoggerFactory, ProcessorFormatter
 
 from app.core.config import settings
 
+SENSITIVE_FIELDS = {
+    "user_id",
+    "slack_id",
+    "team_id",
+    "enterprise_id",
+    "state",
+}
+
 
 def setup_logging():
     shared_processors = [
@@ -35,17 +43,14 @@ def setup_logging():
 
 def get_environment_logging_config(processors: list):
     if settings.DEBUG:
-        processors.extend(
-            [
-                structlog.processors.CallsiteParameterAdder(
-                    {
-                        structlog.processors.CallsiteParameter.LINENO,
-                    }
-                ),
-            ]
+        processors.append(
+            structlog.processors.CallsiteParameterAdder(
+                {structlog.processors.CallsiteParameter.LINENO}
+            )
         )
         return processors, structlog.dev.ConsoleRenderer(), logging.DEBUG
 
+    processors.insert(0, obfuscate_sensitive_fields)
     return processors, structlog.processors.JSONRenderer(), logging.INFO
 
 
@@ -71,3 +76,18 @@ def configure_std_logging_and_uvicorn(
         logger = logging.getLogger(name)
         logger.handlers.clear()
         logger.propagate = True
+
+
+def obfuscate_sensitive_fields(logger, method_name, event_dict):
+    for field in SENSITIVE_FIELDS:
+        value = event_dict.get(field)
+        if value:
+            event_dict[field] = obfuscate_data(value)
+    return event_dict
+
+
+def obfuscate_data(value: str, prefix: int = 3, suffix: int = 2) -> str:
+    value = str(value)
+    if len(value) <= prefix + suffix:
+        return "***"
+    return f"{value[:prefix]}***{value[-suffix:]}"
